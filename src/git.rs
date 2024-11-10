ea21ce8dce927846473f293c7b7a8d6e91fc546a
kortgrabb <timherdemark86@gmail.com>
2024-11-10 21:20:52 +0100

    test

--- src/git.rs
+++ src/git.rs
@@ -15,13 +15,40 @@ impl GitInfo {
         let branch = head.as_ref().and_then(|h| h.shorthand());
 
         if let Some(branch_name) = branch {
-            let mut status = String::new();
+            let mut status = Vec::new();
+
             if let Ok(statuses) = self.repo.statuses(None) {
-                if !statuses.is_empty() {
-                    status.push('*');
+                let mut modified = 0;
+                let mut staged = 0;
+                let mut untracked = 0;
+
+                for entry in statuses.iter() {
+                    match entry.status() {
+                        s if s.is_wt_modified() => modified += 1,
+                        s if s.is_index_modified() => staged += 1,
+                        s if s.is_wt_new() => untracked += 1,
+                        _ => {}
+                    }
+                }
+
+                if modified > 0 {
+                    status.push(format!("{}⚡", modified));
+                }
+                if staged > 0 {
+                    status.push(format!("{}✓", staged));
+                }
+                if untracked > 0 {
+                    status.push(format!("{}?", untracked));
                 }
             }
-            format!("[{}{}]", branch_name.yellow(), status.red())
+
+            let status_str = if !status.is_empty() {
+                status.join(" ").red().to_string()
+            } else {
+                String::new()
+            };
+
+            format!("[{} {}]", branch_name.yellow(), status_str)
         } else {
             String::new()
         }
